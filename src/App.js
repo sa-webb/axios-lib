c6a3aedadcb7599fabd020f0edb9ef313281098b
sa-webb <sawebb30@gmail.com>
2020-04-01 22:15:46 -0500

    cleaned

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import axios from 'axios';
 
 async function go() {
   try {
-    const confirmedPromise = axios('http://localhost:5000/us-confirmed-growth-curve/');
-    const recoveredPromise = axios('http://localhost:5000/us-recovered-growth-curve/');
-    const deathsPromise = axios('http://localhost:5000/us-deaths-growth-curve/');
+    const anyPromise1 = axios('http://localhost:5000/endpoint1/');
+    const anyPromise2 = axios('http://localhost:5000/endpoint2/');
+    const anyPromise3 = axios('http://localhost:5000/endpoint3/');
     // await all three promises to come back and destructure the result into their own variables
-    const [confirmed, recovered, deaths] = await Promise.all([
-      confirmedPromise,
-      recoveredPromise,
-      deathsPromise
+    const [result1, result2, result3] = await Promise.all([
+      anyPromise1,
+      anyPromise2,
+      anyPromise3
     ]);
-    console.log(confirmed.data[0].values, recovered.data[0].values, deaths.data[0].values); // cool, {...}, {....}
+    console.log(result1.data[0].values, result2.data[0].values, result3.data[0].values); // cool, {...}, {....}
   } catch (e) {
     console.error(e); // 💩
   }
